75881b45a7e9f9ae5b1015f9274f9d0ed78c652e
mbaracz <yetrrdevofficial@gmail.com>
2022-07-28 13:47:59 +0200

    Add try-with-resources block when using Scanner

--- src/pl/mbaracz/nim/Main.java
+++ src/pl/mbaracz/nim/Main.java
@@ -10,16 +10,16 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-
-        Player player1, player2;
-        if (Config.HUMAN_FIRST_MOVE_WITH_COMPUTER) {
-            player1 = InputUtil.askForPlayer(1, scanner);
-            player2 = Config.GAME_WITH_COMPUTER ? new Player(Config.COMPUTER_NAME, false) : InputUtil.askForPlayer(2, scanner);
-        } else {
-            player1 = Config.GAME_WITH_COMPUTER ? new Player(Config.COMPUTER_NAME, false) : InputUtil.askForPlayer(1, scanner);
-            player2 = InputUtil.askForPlayer(2, scanner);
+        try (Scanner scanner = new Scanner(System.in)) {
+            Player player1, player2;
+            if (Config.HUMAN_FIRST_MOVE_WITH_COMPUTER) {
+                player1 = InputUtil.askForPlayer(1, scanner);
+                player2 = Config.GAME_WITH_COMPUTER ? new Player(Config.COMPUTER_NAME, false) : InputUtil.askForPlayer(2, scanner);
+            } else {
+                player1 = Config.GAME_WITH_COMPUTER ? new Player(Config.COMPUTER_NAME, false) : InputUtil.askForPlayer(1, scanner);
+                player2 = InputUtil.askForPlayer(2, scanner);
+            }
+            new Game(scanner, player1, player2).start();
         }
-        new Game(scanner, player1, player2).start();
     }
 }
